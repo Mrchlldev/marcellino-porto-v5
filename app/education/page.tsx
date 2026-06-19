6e6d598dd530594c3e7bb4f8c48c69d2c90ac86c
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:12:17 +0700

    Upload app/education/page.tsx

---
+++ app/education/page.tsx
@@ -0,0 +1,10 @@
+import { EducationSection } from "@/components/sections/EducationSection";
+import { PageWrapper } from "@/components/ui/PageWrapper";
+
+export default function EducationPage() {
+  return (
+    <PageWrapper>
+      <EducationSection />
+    </PageWrapper>
+  );
+}
