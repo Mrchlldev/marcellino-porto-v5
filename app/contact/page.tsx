234d8bad41223ead948ab43a011bc9c189a5d656
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:12:15 +0700

    Upload app/contact/page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { ContactSection } from "@/components/sections/ContactSection";
+import { PageWrapper } from "@/components/ui/PageWrapper";
+
+export default function ContactPage() {
+  return (
+    <PageWrapper>
+      <ContactSection />
+    </PageWrapper>
+  );
+}
