eb790c365eb3fb1e8bfc873849e6aedb223f4bf7
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:12:27 +0700

    Upload app/skills/page.tsx

---
+++ app/skills/page.tsx
@@ -0,0 +1,10 @@
+import { SkillsSection } from "@/components/sections/SkillsSection";
+import { PageWrapper } from "@/components/ui/PageWrapper";
+
+export default function SkillsPage() {
+  return (
+    <PageWrapper>
+      <SkillsSection />
+    </PageWrapper>
+  );
+}
