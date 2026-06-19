c17d39e3e6206488e6d55045c2853e6af7c3be39
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-19 17:12:25 +0700

    Upload app/projects/page.tsx

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import { ProjectsSection } from "@/components/sections/ProjectsSection";
+import { PageWrapper } from "@/components/ui/PageWrapper";
+
+export default function ProjectsPage() {
+  return (
+    <PageWrapper>
+      <ProjectsSection />
+    </PageWrapper>
+  );
+}
